09a6c1e93e7d2f5264a8dee1616dcf23ad1d83c7
BaayXaliil <ibrahima.diop@idev-ic.net>
2022-07-05 22:21:38 +0000

    Home page ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: "full", redirectTo: "/home" },
-  { path: "home", component: HomeComponent }
+  { path: "home", component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
