060e5daa836d985291b91dc8438b359bef37868c
BaayXaliil <ibrahima.diop@idev-ic.net>
2022-07-07 11:00:29 +0000

    Review codes ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  monthly: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onChange(e: any) {
+    this.monthly = e.target.checked;
+    
+  }
 }
